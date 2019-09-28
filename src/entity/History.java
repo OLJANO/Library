70cd01854ab520489539f8e56475e9632090bb44
OLGA <oliva222@hotmail.com>
2019-09-29 02:04:44 +0300

    29/09/2019

---
+++ src/entity/History.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author user
+ */
+public class History {
+    private Book book;
+    private Reader reader;
+    private Date takeOn;
+    private Date returnDate;
+
+    public History() {
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public Date getTakeOn() {
+        return takeOn;
+    }
+
+    public void setTakeOn(Date takeOn) {
+        this.takeOn = takeOn;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "History{" + "book=" + book + ", reader=" + reader + ", takeOn=" + takeOn + ", returnDate=" + returnDate + '}';
+    }
+
+}
---
+++ src/entity/Reader.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author user
+ */
+public class Reader {
+    private String name;
+    private String lastname;
+    private String email;
+
+    public Reader() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" + "name=" + name + ", lastname=" + lastname + ", email=" + email + '}';
+    }
+
+}
